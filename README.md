053e0ceebdc5694efcbad47585726a31c6de0836
Matt Seng <mmseng@users.noreply.github.com>
2025-03-16 14:37:59 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# Test-AdDnsOverTime
-Script to monitor changes in the IPs of a set of AD DNS records over time.
+# Summary
+Script to monitor changes in the IPs of a set of AD DNS records over time.  
+
+As an example of the purpose, the original use case was to monitor the IPs returned by the AD DNS records for a cart of laptops, to see when and how often they would switch between wired and wireless. The data gathered was useful in optimizing device power management and network adapter priority.  
+
+# Requirements
+- PowerShell 7+ for the use of `ForEach-Object -Parallel` within the `Ping-All` module.
+- [Ping-All](https://github.com/engrit-illinois/Ping-All) module
+
+# Behavior
+This script pings a set of AD computer names a given number of times with a given interval in between (e.g. once a minute for 24 hours), and outputs the resulting IPv4 IPs to the console and to a CSV file.  
+
+The script accepts 2 IP ranges, and will color code the console output depending on which range each IP falls in. This is just to make it easier to see changes over time at a glance without actually reading each IP. If desired, the raw CSV output can be similarly and easily color coded in Excel using conditional formatting.  
+
+Note that IP ranges are accepted and output with leading zeros for padding, such that all IPs are always displayed with the same 15-character width.  
+
+# Usage
+1. Download `Test-AdDnsOverTime.psm1` to the appropriate subdirectory of your PowerShell [modules directory](https://github.com/engrit-illinois/how-to-install-a-custom-powershell-module).
+2. Download [Ping-All](https://github.com/engrit-illinois/Ping-All) to the appropriate subdirectory of your PowerShell [modules directory](https://github.com/engrit-illinois/how-to-install-a-custom-powershell-module).
+3. Run `Test-AdDnsOverTime` using the documentation below.
+
+# Example
+```powershell
+$comps = Get-ADComputer -Filter "name -like 'lt-cart01-*'" | Sort "Name" | Select -ExpandProperty "Name" | Select -First 10
+Test-AdDnsOverTime -Computer $comps -IpRange1 "010.000.000.*" -IpRange2 "172.016.000.*"
+```
+
+<img title='Screenshot of example console output' alt='Screenshot of example console output' src='./example.png' />
+
+# Parameters
+WIP
+
+# Notes
+By mseng3. See my other projects here: https://github.com/mmseng/code-compendium.
---
+++ Test-AdDnsOverTime.psm1
@@ -0,0 +1,146 @@
+function Test-AdDnsOverTime {
+
+	param(
+		[Parameter(Mandatory=$true)]
+		[string[]]$Computer,
+		
+		[Parameter(Mandatory=$true)]
+		[string]$IpRange1,
+		[Parameter(Mandatory=$true)]
+		[string]$IpRange2,
+		
+		[int]$Loops = 1440,
+		[int]$IntervalSeconds = 60,
+		[int]$PingCount = 2,
+		[int]$PingTimeoutSeconds = 2,
+
+		[string]$IpRange1Fc = "green",
+		[string]$IpRange2Fc = "yellow",
+		[string]$IpUnknownRangeBc = "red",
+		
+		[string]$LogDir = "c:\engrit\logs"
+	)
+
+	$ts = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+	$logName = "cbtf-cart01-nic-interface-priority-test_$($ts)"
+	$csv = "$LogDir\$($logName).csv"
+
+	function csv($line) {
+		$line | Out-File $csv -Append
+	}
+
+	function log {
+		param(
+			[string]$Msg,
+			[ValidateScript({[System.Enum]::GetValues([System.ConsoleColor]) -contains $_})]
+			[string]$FC = (get-host).ui.rawui.ForegroundColor, # foreground color
+			[ValidateScript({[System.Enum]::GetValues([System.ConsoleColor]) -contains $_})]
+			[string]$BC = (get-host).ui.rawui.BackgroundColor, # background color
+			[switch]$NoTs,
+			[switch]$NoNl
+		)
+		
+		if(-not $NoTs) {
+			$ts = Get-Date -Format "HH:mm:ss"
+			$Msg = "[$ts] $Msg"
+		}
+		
+		$params = @{
+			Object = $Msg
+		}
+		if($NoNl) { $params.NoNewline = $true }
+		if($FC) { $params.ForegroundColor = $FC }
+		if($BC) { $params.BackgroundColor = $BC }
+		
+		Write-Host @params
+	}
+	
+	if($Computer) {
+		$compsCount = @($Computer).count
+	}
+
+	# Header for console output
+	# cbtf-cart01-01  | 
+	# xxx.xxx.xxx.xxx | 
+	$compsLine = $comps -join "  | "
+	log $compsLine
+	$underline = ""
+	$comps | ForEach-Object {
+		$underline += "----------------|-"
+	}
+	$underline = $underline.Substring(0,$underline.length -4)
+	log $underline
+	
+	# Header for CSV
+	$compsLineCsv = $comps -join ","
+	csv $compsLineCsv
+	
+	# Loop once for each interval
+	@(0..$Loops) | ForEach-Object {
+		
+		# Get results of pings
+		$result = $null
+		$result = Ping-All $comps -Quiet -PassThru -Count $PingCount -TimeoutSeconds $PingTimeoutSeconds | Sort "TargetName"
+		
+		# Console output timestamp for current line
+		log -NoNl
+		
+		# Loop through each IP
+		$i = 0
+		$ipsPadded = $null
+		$ipsPadded = $result.IPv4_IP | ForEach-Object {
+			$ip = $_
+			
+			# Format the IP to be a consistent length
+			$ipPadded = "               "
+			if($ip) {
+				$ipParts = $ip.Split('.')
+				$ipPartsPadded = $ipParts | ForEach-Object {
+					$part = $_
+					$part.PadLeft(3, '0')
+				}
+				$ipPadded = $ipPartsPadded -join '.'
+			}
+			
+			# Output line to console
+			$params = @{
+				Msg = $ipPadded
+				NoNl = $true
+				NoTs = $true
+			}
+			if($ipPadded -ne "               ") {
+				if($ipPadded -like $IpRange1) {
+					$params.FC = $IpRange1Fc
+				}
+				elseif($ipPadded -like $IpRange2) {
+					$params.FC = $IpRange2Fc
+				}
+				else {
+					$params.BC = $IpUnknownRangeBc
+				}
+			}
+			
+			log @params
+			
+			if($i -lt ($compsCount -1)) {
+				log " | " -NoNl -NoTs
+			}
+			else {
+				log "" -NoTs
+			}
+			$i += 1
+			
+			# Return IPs for CSV output
+			$ipPadded
+		}
+		
+		# Output CSV line
+		$ipLineCsv = $ipsPadded.Replace(" ","") -join ","
+		csv $ipLineCsv
+		
+		# Wait for next loop
+		Start-Sleep -Seconds $IntervalSeconds
+	}
+
+	log "EOF"
+}
